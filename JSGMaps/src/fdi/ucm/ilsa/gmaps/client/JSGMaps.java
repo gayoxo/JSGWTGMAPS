7c1a535264f642290eaae18d309f7af1f6282ddb
Joaquin Gayoso Cabada <jgayoso@ucm.es>
2017-12-13 18:09:05 +0100

    Ala a casa

--- JSGMaps/src/fdi/ucm/ilsa/gmaps/client/JSGMaps.java
+++ JSGMaps/src/fdi/ucm/ilsa/gmaps/client/JSGMaps.java
@@ -1,146 +1,97 @@
 package fdi.ucm.ilsa.gmaps.client;
 
-import fdi.ucm.ilsa.gmaps.shared.FieldVerifier;
+import java.util.ArrayList;
+
+import com.google.gwt.core.client.Duration;
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.event.dom.client.KeyUpEvent;
-import com.google.gwt.event.dom.client.KeyUpHandler;
-import com.google.gwt.user.client.rpc.AsyncCallback;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.DialogBox;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
 import com.google.gwt.user.client.ui.RootPanel;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
+
+import fdi.ucm.server.interconect.model.DocumentCompleteJSON;
+import fdi.ucm.server.interconect.model.DocumentsJSON;
+import fdi.ucm.server.interconect.model.GrammarJSON;
+import fdi.ucm.server.interconect.model.OperationalValueJSON;
+
+
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
 public class JSGMaps implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network " + "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
-
+	
+	private final static String iconvalue="gmaps.png";
+	
+	static {
+        export();
+    }
+	
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
-		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
-
-		// We can add style names to widgets
-		sendButton.addStyleName("sendButton");
-
-		// Add the nameField and sendButton to the RootPanel
-		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
-		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
-
-		// Focus the cursor on the name field when the app loads
-		nameField.setFocus(true);
-		nameField.selectAll();
-
-		// Create the popup dialog box
-		final DialogBox dialogBox = new DialogBox();
-		dialogBox.setText("Remote Procedure Call");
-		dialogBox.setAnimationEnabled(true);
-		final Button closeButton = new Button("Close");
-		// We can set the id of a widget by accessing its Element
-		closeButton.getElement().setId("closeButton");
-		final Label textToServerLabel = new Label();
-		final HTML serverResponseLabel = new HTML();
-		VerticalPanel dialogVPanel = new VerticalPanel();
-		dialogVPanel.addStyleName("dialogVPanel");
-		dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
-		dialogVPanel.add(textToServerLabel);
-		dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
-		dialogVPanel.add(serverResponseLabel);
-		dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
-		dialogVPanel.add(closeButton);
-		dialogBox.setWidget(dialogVPanel);
-
-		// Add a handler to close the DialogBox
-		closeButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				dialogBox.hide();
-				sendButton.setEnabled(true);
-				sendButton.setFocus(true);
-			}
-		});
-
-		// Create a handler for the sendButton and nameField
-		class MyHandler implements ClickHandler, KeyUpHandler {
-			/**
-			 * Fired when the user clicks on the sendButton.
-			 */
-			public void onClick(ClickEvent event) {
-				sendNameToServer();
-			}
-
-			/**
-			 * Fired when the user types in the nameField.
-			 */
-			public void onKeyUp(KeyUpEvent event) {
-				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-					sendNameToServer();
-				}
-			}
-
-			/**
-			 * Send the name from the nameField to the server and wait for a response.
-			 */
-			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
-				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
-
-				// Then, we send the input to the server.
-				sendButton.setEnabled(false);
-				textToServerLabel.setText(textToServer);
-				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer, new AsyncCallback<String>() {
-					public void onFailure(Throwable caught) {
-						// Show the RPC error message to the user
-						dialogBox.setText("Remote Procedure Call - Failure");
-						serverResponseLabel.addStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(SERVER_ERROR);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-
-					public void onSuccess(String result) {
-						dialogBox.setText("Remote Procedure Call");
-						serverResponseLabel.removeStyleName("serverResponseLabelError");
-						serverResponseLabel.setHTML(result);
-						dialogBox.center();
-						closeButton.setFocus(true);
-					}
-				});
-			}
-		}
-
-		// Add a handler to send the name to the server
-		MyHandler handler = new MyHandler();
-		sendButton.addClickHandler(handler);
-		nameField.addKeyUpHandler(handler);
+		//TODO
+		/*
+		 * -Crea unas 
+		 */
+		
+		DocumentsJSON DJ=new DocumentsJSON(01l, "coordenadasTest", "", 01l, new ArrayList<Long>(), new ArrayList<OperationalValueJSON>(), "");
+		
+		ArrayList<GrammarJSON> grammars=new ArrayList<GrammarJSON>();
+		
+		
+		DocumentCompleteJSON text=new DocumentCompleteJSON(DJ,grammars);
+		
+		RootPanel RT=RootPanel.get();
+		
+		VerticalPanel EjemploPanel=new VerticalPanel();
+		
+		RT.add(EjemploPanel);
+		
+		String RandomIdVars = Long.toString(Math.abs(Math.round(Double.valueOf(Duration.currentTimeMillis()))));
+		
+		
+		setContextDiv(EjemploPanel, RandomIdVars);
+		setVariableBase(text, RandomIdVars);
+		
 	}
+
+	 private static native void export() /*-{
+     $wnd.JSGMapsgetIconData = @fdi.ucm.ilsa.gmaps.client.JSGMaps::getIcon();
+     $wnd.JSGMapsgetParamData = @fdi.ucm.ilsa.gmaps.client.JSGMaps::getIcon();
+     $wnd.JSGMapssetContextView = @fdi.ucm.ilsa.gmaps.client.JSGMaps::setContextView(Ljava/lang/String;JIZ);
+     $wnd.JSGMapssetContextEdit = @fdi.ucm.ilsa.gmaps.client.JSGMaps::getIcon();
+     $wnd.JSGMapspersist = @fdi.ucm.ilsa.gmaps.client.JSGMaps::getIcon();
+ }-*/;
+	 
+	 
+	 public static native void setVariableBase(DocumentCompleteJSON DocumentoExpandido, String idrandomdoct) /*-{
+		
+		
+		$wnd.tmp=DocumentoExpandido;
+		$wnd.str = '$wnd.DocExpand'+idrandomdoct +' = $wnd.tmp';
+		console.log($wnd.str);
+		eval($wnd.str)
+
+	}-*/;
+	
+	
+	public static native void setContextDiv(Panel context, String idrandomdoct) /*-{
+	
+	
+	$wnd.tmp=context;
+	$wnd.str = '$wnd.VDocExpand'+idrandomdoct +' = $wnd.tmp';
+	console.log($wnd.str);
+	eval($wnd.str)
+
+}-*/;
+	 
+	 
+	 private static String getIcon() {
+	    	return iconvalue;
+	    }
+	 
+	 private static String setContextView(String randomIdVars, long contextId, int Height, boolean isgrammar) {
+	    	return iconvalue;
+	    }
 }
